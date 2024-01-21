7ec6e9fdc5d6bcdf03f0e40db05cbb7533523c43
Pierre <prrvchr@gmail.com>
2024-01-21 16:15:06 +0100

    new version 1.1.1

--- source/mContactOOo/service/pythonpath/mcontact/provider.py
+++ source/mContactOOo/service/pythonpath/mcontact/provider.py
@@ -75,11 +75,7 @@ class Provider(ProviderBase):
         parameter = self._getRequestParameter(user.Request, 'getBooks')
         response = user.Request.execute(parameter)
         if not response.Ok:
-            cls, mtd = 'Provider', 'initAddressbooks()'
-            code = response.StatusCode
-            msg = response.Text
-            response.close()
-            raise getSqlException(self._ctx, source, 1006, 1601, cls, mtd, parameter.Name, code, user.Name, msg)
+            self.raiseForStatus(response, source, 'initAddressbooks()', 1006, parameter.Name, user.Name, parameter.Url)
         iterator = self._parseAllBooks(response)
         self.initUserBooks(source, database, user, iterator)
 
@@ -87,11 +83,7 @@ class Provider(ProviderBase):
         parameter = self._getRequestParameter(user.Request, 'getGroups')
         response = user.Request.execute(parameter)
         if not response.Ok:
-            cls, mtd = 'Provider', 'initUserGroups()'
-            code = response.StatusCode
-            msg = response.Text
-            response.close()
-            raise getSqlException(self._ctx, source, 1006, 1671, cls, mtd, parameter.Name, code, user.Name, msg)
+            self.raiseForStatus(response, source, 'initUserGroups()', 1006, parameter.Name, user.Name, parameter.Url)
         iterator = self._parseGroups(response)
         remove, add = database.initGroups(book, iterator)
         print("Provider._initAddressbookGroups() Add: %s - Remove: %s" % (add, remove))
@@ -107,11 +99,7 @@ class Provider(ProviderBase):
         parameter = self._getRequestParameter(request, 'getUser')
         response = request.execute(parameter)
         if not response.Ok:
-            cls, mtd = 'Provider', '_getNewUserId'
-            code = response.StatusCode
-            msg = response.Text
-            response.close()
-            raise getSqlException(self._ctx, source, 1006, 1643, cls, mtd, parameter.Name, code, name, parameter.Url, msg)
+            self.raiseForStatus(response, source, '_getNewUserId()', 1006, parameter.Name, name, parameter.Url)
         userid = self._parseUser(response)
         return userid
 
